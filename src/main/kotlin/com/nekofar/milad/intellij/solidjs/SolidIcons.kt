d5ef01491c535caa8bd81edec11da212b07e03cd
Milad Nekofar <milad@nekofar.com>
2023-04-05 17:06:10 +0330

    refactor: update plugin icon and add extra icons

---
+++ src/main/kotlin/com/nekofar/milad/intellij/solidjs/SolidIcons.kt
@@ -0,0 +1,11 @@
+package com.nekofar.milad.intellij.solidjs
+
+import com.intellij.openapi.util.IconLoader
+
+object SolidIcons {
+    @JvmField
+    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
+
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/solid.svg", javaClass)
+}
